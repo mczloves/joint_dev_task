66cde3c5fba829543fa2e81cf5052a454ed5d2fc
mczloves <mczloves1@yahoo.co.jp>
2021-11-13 16:37:01 +0900

    Ruby応用課題17,18

--- task.rb
+++ task.rb
@@ -167,7 +167,18 @@ end
 
 class UserQ17
   # 以下に回答を記載
+  def initialize(name:, age:, gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
 
+  def info
+     puts "名前：#{@name}"
+     puts "年齢：#{@age}"
+     puts "性別：#{@gender}"
+  end
+  
 end
 
 def q17
@@ -183,6 +194,19 @@ end
 class UserQ18
   # 以下に回答を記載
 
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    if @age >= 18
+    puts "こんにちは,#{@name}と申します。宜しくお願いいたします。"
+    else
+    puts "はいさんまいど〜、#{@name}です！！！"
+    end
+
+  end
 end
 
 def q18
